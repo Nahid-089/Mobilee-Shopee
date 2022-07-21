d6d80ec4880fef3d0a7a9e250092d3f0abab2e98
Nahid-089 <85131542+Nahid-089@users.noreply.github.com>
2022-07-22 04:02:29 +0600

    Added Some Features

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -34,6 +34,37 @@ $(document).ready(function () {
     $grid.isotope({
       filter: filterValue
     });
+  }); // New Phones owl carousel 
+
+  $("#new-phones .owl-carousel").owlCarousel({
+    loop: true,
+    nav: false,
+    dots: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      },
+      1000: {
+        items: 5
+      }
+    }
+  }); // blogs owl carousel
+
+  $("#blogs .owl-carousel").owlCarousel({
+    loop: true,
+    nav: false,
+    dots: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      }
+    }
   });
 });
 //# sourceMappingURL=index.dev.js.map
--- index.js
+++ index.js
@@ -25,7 +25,7 @@ $("#top-sale .owl-carousel").owlCarousel({
         }
 
     }
-})
+});
 
 //isotope filter
 var $grid=$(".grid").isotope({
@@ -38,5 +38,41 @@ $(".button-group").on("click","button",function(){
     var filterValue =$(this).attr('data-filter');
     $grid.isotope({filter: filterValue})
 })
+
+     // New Phones owl carousel 
+$("#new-phones .owl-carousel").owlCarousel({
+    loop:true,
+    nav:false,
+    dots:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+
+    }
+});
+
+// blogs owl carousel
+
+$("#blogs .owl-carousel").owlCarousel({
+    loop:true,
+    nav:false,
+    dots:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        
+    }
+});
 
 })
