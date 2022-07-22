4a448bcaf10f00686b4201f8042e8a4e12f4b45b
Nahid-089 <85131542+Nahid-089@users.noreply.github.com>
2022-07-23 05:29:54 +0600

    cart.html

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -69,10 +69,12 @@ $(document).ready(function () {
   // product qty section
 
   var $qty_up = $(".qty .qty-up");
-  var $qty_down = $(".qty .qty-down");
-  var $input = $(".qty .qty_input"); // click on qty up button
+  var $qty_down = $(".qty .qty-down"); //let $input = $(".qty .qty_input");
+  // click on qty up button
 
   $qty_up.click(function (e) {
+    var $input = $(".qty_input[data-id='".concat($(this).data("id"), "']"));
+
     if ($input.val() >= 1 && $input.val() <= 9) {
       $input.val(function (i, oldval) {
         return ++oldval;
@@ -81,6 +83,8 @@ $(document).ready(function () {
   }); // click on qty down button
 
   $qty_down.click(function (e) {
+    var $input = $(".qty_input[data-id='".concat($(this).data("id"), "']"));
+
     if ($input.val() > 1 && $input.val() <= 10) {
       $input.val(function (i, oldval) {
         return --oldval;
